87214b88f981488979061ec1858b80d1904d1687
magnus <magnus.wahlstrand@gmail.com>
2023-04-16 10:09:07 +0200

    disable zoom

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -5,17 +5,17 @@ import type { ManifestOptions } from "vite-plugin-pwa"
  * Defines the default SEO configuration for the website.
  */
 export const seoConfig = {
-	baseURL: "https://example.com", // Change this to your production URL.
+	baseURL: "https://oskar.vercel.app", // Change this to your production URL.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to be your website's description.
+		"Oskar lär sig", // Change this to be your website's description.
 	type: "website",
 	image: {
-		url: "https://picsum.photos/1200/630", // Change this to your website's thumbnail.
+		url: "https://upload.wikimedia.org/wikipedia/commons/7/76/Swedish_ambulance_in_Lund.jpg", // Change this to your website's thumbnail.
 		alt: "OpenGraph thumbnail description.", // Change this to your website's thumbnail description.
 		width: 1200,
 		height: 630
 	},
-	siteName: "Astro PWA Starter", // Change this to your website's name,
+	siteName: "Oskar lär sig", // Change this to your website's name,
 	twitter: {
 		card: "summary_large_image"
 	}
@@ -25,10 +25,10 @@ export const seoConfig = {
  * Defines the configuration for PWA webmanifest.
  */
 export const manifest: Partial<ManifestOptions> = {
-	name: "Astro PWA Starter", // Change this to your website's name.
-	short_name: "Astro PWA Starter", // Change this to your website's short name.
+	name: "Oskar lär sig", // Change this to your website's name.
+	short_name: "Oskar lär sig", // Change this to your website's short name.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to your websites description.
+		"Oskar lär sig", // Change this to your websites description.
 	theme_color: "#30E130", // Change this to your primary color.
 	background_color: "#ffffff", // Change this to your background color.
 	display: "minimal-ui",
