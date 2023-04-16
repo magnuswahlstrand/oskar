b3976551939ee7489c8cc44c9076564798f27bed
magnus <magnus.wahlstrand@gmail.com>
2023-04-16 19:21:20 +0200

    add sound icon

--- src/components/ImageWithSound.tsx
+++ src/components/ImageWithSound.tsx
@@ -1,4 +1,3 @@
-// @flow
 import * as React from 'react';
 
 type Props = {
@@ -11,24 +10,26 @@ export function ImageWithSound(props: Props) {
 
 	const play = () => {
 		console.log('play')
-		if(!props.audio)
+		if (!props.audio)
 			return;
 		new Audio(props.audio).play();
 	}
 
-	// const play = () => {
-	// 	console.log('play')
-	// 	alert('hi')
-	// }
-
 	return (
 		<div
+			className={"relative"}
 			onClick={play}>
 			<img
 				src={props.url}
 				alt={props.name}
-				className="rounded-lg"
+				className={"rounded-lg" + (props.audio ? " cursor-pointer" : "")}
 			/>
+			{props.audio &&
+				<div
+					className="absolute top-2 left-2 flex items-center justify-center w-8 h-8 bg-gray-200 bg-opacity-40	rounded-full p-1">
+					<img src={"speaker.svg"} alt={"has audio"}/>
+				</div>
+			}
 		</div>
 	);
 };
