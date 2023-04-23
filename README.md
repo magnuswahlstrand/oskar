43416456eff87ebfb2c35cacbc62b6bd9e3299eb
magnus <magnus.wahlstrand@gmail.com>
2023-04-23 08:09:04 +0200

    refactor a bit

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 A simple app for my son. Built using [astro.build](https://astro.build/) with the [astro-pwa-starter](https://github.com/shaunchander/astro-pwa-starter) theme.
 
+See demo here: [oskar.vercel.app](https://oskar.vercel.app/)
 
 
--- src/components/ImageWithSound.tsx
+++ src/components/ImageWithSound.tsx
@@ -6,46 +6,61 @@ type Props = {
 	audio?: string | [string, string]
 };
 
-function playDouble(url: string, url2: string) {
-	const first = new Audio(url);
-	const second = new Audio(url2);
-	first.addEventListener('ended', function () {
-		second.play();
+
+function playAudio(url: string): Promise<void> {
+	return new Promise((resolve, reject) => {
+		const audio = new Audio(url);
+		audio.addEventListener('ended', function () {
+			console.log('ended')
+			resolve()
+		})
+		audio.addEventListener('error', function () {
+			console.log('ended')
+			reject()
+		})
+		audio.play();
 	})
-	first.play();
+}
+
+function SpeakerIcon(props: { playing: boolean }) {
+	return <div
+		className={`absolute top-2 left-2 flex items-center justify-center w-8 h-8 bg-gray-200 rounded-full p-1 ${props.playing ? "bg-opacity-100" : "bg-opacity-40"}`}>
+		<img src={"speaker.svg"} alt={"has audio"}/>
+	</div>;
 }
 
 export function ImageWithSound(props: Props) {
-
+	const [playing, setPlaying] = React.useState(false);
 	const play = () => {
-		console.log('play')
-		if (!props.audio) {
-			// Play  nothing
-		} else if (typeof props.audio === 'string') {
+		if (playing || !props.audio) return
+
+		setPlaying(true)
+		if (typeof props.audio === 'string') {
 			// Play one audio clips
-			new Audio(props.audio).play();
+			playAudio(props.audio)
+				.finally(() => setPlaying(false))
 		} else {
 			// Play two audio clips
-			playDouble(props.audio[0], props.audio[1])
+			playAudio(props.audio[0])
+				.then(() => playAudio(props.audio[1]))
+				.finally(() => setPlaying(false))
 		}
 	}
+
+	const imgClasses = "rounded-lg" + props.audio ? " cursor-pointer" : ""
 
 	return (
 		<div
 			className={"relative"}
-			onMouseDown={play}
-			// onClick={play}
+			onMouseDown={play ? play : undefined}
 		>
 			<img
 				src={props.url}
 				alt={props.name}
-				className={"rounded-lg" + (props.audio ? " cursor-pointer" : "")}
+				className={imgClasses}
 			/>
 			{props.audio &&
-				<div
-					className="absolute top-2 left-2 flex items-center justify-center w-8 h-8 bg-gray-200 bg-opacity-40	rounded-full p-1">
-					<img src={"speaker.svg"} alt={"has audio"}/>
-				</div>
+				<SpeakerIcon playing={playing}/>
 			}
 		</div>
 	);
